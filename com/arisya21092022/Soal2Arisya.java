9d2a5c54eec35b16d3946b08a9156c701adf6cda
Arisya18 <rianiarisya@gmail.com>
2022-11-04 09:29:40 +0700

    folder com

---
+++ com/arisya21092022/Soal2Arisya.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.arisya21092022;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Soal2Arisya {
+    public static void main(String[] args) {
+        int number1 = 10; 
+        int number2 = 20;
+        int number3 = 45;
+        int average;
+        average= number1 + number2 + number3/3;
+        
+        System.out.println("number 1 = " +number1);
+        System.out.println("number 2 = " +number2);
+        System.out.println("number 1 = " +number3);
+        System.out.println("Average is = " +average);
+        
+        
+    }
+}
---
+++ com/arisya21092022/VariableSamplesArisya.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.arisya21092022;
+
+/**
+ *
+ * @author ASUS
+ */
+public class VariableSamplesArisya {
+    public static void main(String[] args) {
+        boolean result = true;
+        char option;
+        option = 'C';
+        double grade = 0.0;
+        System.out.println(result);
+        System.out.println(option);
+        System.out.println(grade);
+    }
+}
