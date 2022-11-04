a88b5a21275a2814e6d50080353e16f9073ca6f9
Arisya18 <rianiarisya@gmail.com>
2022-11-04 09:36:01 +0700

    modul data diri dan perkenalan

---
+++ arisya12092022/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arisya12092022;
+
+/**
+    Program : Data Diri.java
+    Pembuat : Arisya Riani
+    Tanggal : 12 September 2022
+    Deskripsi : Membuat Data Diri 
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World\n"+"Pemrograman Berbasis Objek");
+        //System.out.println("Pemrograman Berbasis Objek");
+    }
+}
