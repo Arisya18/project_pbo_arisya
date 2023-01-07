6f7d99eb4340960bfc08a357333cf7a8ad35f2e2
Arisya18 <rianiarisya@gmail.com>
2023-01-07 15:08:14 +0700

    Latihan Modul 3

---
+++ PRAKTEK PBO/arisya12092022/DataDiri.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arisya12092022;
+
+/*  Program : Data Diri.java
+    Pembuat : Arisya Riani
+    Tanggal : 12 September 2022
+    Deskripsi : Membuat Data Diri 
+ */
+public class DataDiri {
+
+    //Nama  :
+    //No Bp :
+    //Prodi   :
+    //Jurusan :
+    //Alamat  :
+    public static void main(String[] args) {
+        System.out.println("Nama\t:Arisya Riani\n"
+                + "No Bp\t:2101091001\n"
+                + "Prodi\t:Manajemen Informatika\n"
+                + "Jurusan\t:Teknologi Informasi\n"
+                + "Alamat\t:Jl.Cupak Tangah");
+    }
+
+}
